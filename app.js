113a382888b031e13d6b39f9e5413a4388753a06
rvrj-patil <67901386+rvrj-patil@users.noreply.github.com>
2020-08-01 19:31:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+const request = require('request');
+var MongoClient = require('mongodb').MongoClient;
+const express = require('express')
+const mongoose = require('mongoose')
+const url = 'mongodb://localhost:27017/mydb';
+const fetch = require('node-fetch');
+const app = express()
+
+MongoClient.connect(url,{useNewUrlParser:true,useUnifiedTopology: true }, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  app.use(express.json())
+
+request({
+  url:"https://randomuser.me/api/?inc=gender,name,location,email,dob,registered,phone,cell,id,nat&nat=NL&noinfo",
+  json:true
+},(err,response,body)=>{
+  
+  var test = JSON.stringify(body, undefined, 4);
+  let test1 = body
+ // console.log(body.results[0].name.first);
+    // dbo.collection("customers").insertOne(body.results[0], function(err, res) {
+    // if (err) throw err;
+    // console.log("1 document inserted");
+    // db.close();
+    //   }); 
+    })
+});
+
+
+MongoClient.connect(url,{ useUnifiedTopology: true }, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  //var query = { dob:{age:{ $gt: 20 }} };
+dbo.collection("customers").find({ gender:"male",$and: [ { "dob.age": { $gt: 0, $lt: 30 }}]}).count(function(err, result) {
+    if (err) throw err;
+    //const test = JSON.stringify(result, undefined, 2) 
+    console.log("0-30:",result)
+    db.close();
+  });
+  dbo.collection("customers").find({ gender:"female",$and: [ { "dob.age": { $gt: 30, $lt: 50 }}]}).count(function(err, result) {
+    if (err) throw err;
+    //const test = JSON.stringify(result, undefined, 2) 
+    console.log("0-50:",result)
+    db.close();
+  });
+  dbo.collection("customers").find({ $and: [ { "dob.age": { $gt: 50 }}]}).count(function(err, result) {
+    if (err) throw err;
+    //const test = JSON.stringify(result, undefined, 2) 
+    console.log("50-above:",result)
+    db.close();
+  });
+});
+
+app.listen(9000,()=>{
+    console.log("server is running")
+})
